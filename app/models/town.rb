e71400b7460813abc62955c9ee1f432d7bc0ae2f
Aurélien <aurelien.blais@whataniceplace.com>
2018-03-17 09:53:36 +0100

    Add nil check

--- app/models/town.rb
+++ app/models/town.rb
@@ -5,7 +5,7 @@ class Town < ActiveRecord::Base
 
   def get_geocoding
     town           = Nominatim.search.city(name).country('France').limit(1).address_details(true).first
-    self.zipcode   = town.address.postcode
+    self.zipcode   = town.address&.postcode
     self.latitude  = town.latitude
     self.longitude = town.longitude
   end
